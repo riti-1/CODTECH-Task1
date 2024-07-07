803f710ff6637af4db888d93f897631269e1e026
Ritismita Bal <balritismita@gmail.com>
2024-07-07 09:51:29 +0530

    Add files via upload

---
+++ ConnectionProvide.java
@@ -0,0 +1,28 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author ritis
+ */
+public class ConnectionProvide {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","password");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    
+}
+}
